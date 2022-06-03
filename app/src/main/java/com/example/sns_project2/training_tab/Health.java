2394ba71ee12d8fc0141a8fbdfa599fe94266240
heojuseon <hjs2683@naver.com>
2022-06-04 02:28:47 +0900

    유튜브 영상 출력

--- app/src/main/java/com/example/sns_project2/training_tab/Health.java
+++ app/src/main/java/com/example/sns_project2/training_tab/Health.java
@@ -2,21 +2,59 @@ package com.example.sns_project2.training_tab;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+
+import androidx.fragment.app.Fragment;
+
 
 import com.example.sns_project2.R;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 
 public class Health extends Fragment {
+    YouTubePlayerView youTubePlayerView;
+    LinearLayout recipeViewLinearLayout;
+
+//    //유튜브 API KEY와 동영상 ID 변수 설정
+//    private static String API_KEY = "AIzaSyCzmawqBqx01jL2nzmdCH4urzpW5ygclmM";
+//
+//    //https://www.youtube.com/watch?v=bn8MSLQ1Gsg&list=PLdER_vr5Q2R7OaI58ksEBkn9k56pHo-AT&index=48 ▶ 유튜브 동영상 v= 다음 부분이 videoId
+//    private static String videoId = "bn8MSLQ1Gsg&list=PLdER_vr5Q2R7OaI58ksEBkn9k56pHo-AT&index=48";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_health, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_health, container, false);
+
+        recipeViewLinearLayout = rootView.findViewById(R.id.recipe_view_linearLayout);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.bottomMargin = 30;
+
+        youTubePlayerView = new YouTubePlayerView(Health.this.getContext());
+        youTubePlayerView.setLayoutParams(params);
+        recipeViewLinearLayout.addView(youTubePlayerView);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(YouTubePlayer youTubePlayer) {
+                String videoId = "wmRVz6GfHDs";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+
+
+        return rootView;
     }
+
+
 }
--- app/src/main/java/com/example/sns_project2/training_tab/Running.java
+++ app/src/main/java/com/example/sns_project2/training_tab/Running.java
@@ -1,23 +1,95 @@
 package com.example.sns_project2.training_tab;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.sns_project2.R;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+import com.google.android.youtube.player.YouTubeThumbnailLoader;
+import com.google.android.youtube.player.YouTubeThumbnailView;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 
-public class Running extends Fragment {
+public class Running extends Fragment implements YouTubeThumbnailView.OnInitializedListener {
 
+    YouTubeThumbnailView youTubeThumbnailView;
+    YouTubeThumbnailLoader youTubeThumbnailLoader;
+
+
+    public String ID ="lKwZ2DU4P-A";
+
+
+    public static final String API_KEY = "AIzaSyCzmawqBqx01jL2nzmdCH4urzpW5ygclmM";
+
+
+    //https://www.youtube.com/watch?v=lKwZ2DU4P-A
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_running, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_running, container, false);
+
+        youTubeThumbnailView = rootView.findViewById(R.id.thumbnail);
+        youTubeThumbnailView.initialize(API_KEY, Running.this);
+
+        setListener();
+
+        return rootView;
+    }
+
+    private void setListener() {
+        youTubeThumbnailView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent();
+//                intent = YouTubeStandalonePlayer.createPlaylistIntent(getActivity(), API_KEY, ID, 0, 0, true, false);
+//                intent.resolveActivity(getContext().getPackageManager());
+//                startActivity(intent);
+                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.youtube.com/watch?v=lKwZ2DU4P-A"))
+                        .setPackage("com.google.android.youtube"));
+            }
+        });
+    }
+
+
+    @Override
+    public void onInitializationSuccess(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader thumbnailLoader) {
+
+        youTubeThumbnailLoader = thumbnailLoader;
+        youTubeThumbnailLoader.setVideo(ID);
+
+        thumbnailLoader.setOnThumbnailLoadedListener(new YouTubeThumbnailLoader.OnThumbnailLoadedListener() {
+            @Override
+            public void onThumbnailLoaded(YouTubeThumbnailView youTubeThumbnailView, String s) {
+                Log.d(TAG, "onThumbnailLoaded");
+            }
+
+            @Override
+            public void onThumbnailError(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader.ErrorReason errorReason) {
+                Log.d(TAG, "onThumbnailfail");
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubeThumbnailView youTubeThumbnailView, YouTubeInitializationResult youTubeInitializationResult) {
+        Log.d("에러", "에러");
     }
 }
