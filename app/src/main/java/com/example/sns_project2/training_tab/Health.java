7e9681f3d10e1d3fc8da1de8498b83aaed07ac74
heojuseon <hjs2683@naver.com>
2022-07-13 16:14:45 +0900

    youTubePlayerView, thumbnail 추가

--- app/src/main/java/com/example/sns_project2/training_tab/Health.java
+++ app/src/main/java/com/example/sns_project2/training_tab/Health.java
@@ -19,8 +19,11 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTube
 
 
 public class Health extends Fragment {
-    YouTubePlayerView youTubePlayerView;
-    LinearLayout recipeViewLinearLayout;
+    YouTubePlayerView youTubePlayerView, youTubePlayerView2, youTubePlayerView3, youTubePlayerView4, youTubePlayerView5, youTubePlayerView6;
+
+    LinearLayout recipeViewLinearLayout, recipeViewLinearLayout2, recipeViewLinearLayout3, recipeViewLinearLayout4, recipeViewLinearLayout5,
+            recipeViewLinearLayout6;
+
 
 //    //유튜브 API KEY와 동영상 ID 변수 설정
 //    private static String API_KEY = "AIzaSyCzmawqBqx01jL2nzmdCH4urzpW5ygclmM";
@@ -33,8 +36,8 @@ public class Health extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_health, container, false);
 
+        //1번 영상
         recipeViewLinearLayout = rootView.findViewById(R.id.recipe_view_linearLayout);
-
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
         params.bottomMargin = 30;
@@ -47,11 +50,106 @@ public class Health extends Fragment {
         youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
             @Override
             public void onReady(YouTubePlayer youTubePlayer) {
-                String videoId = "wmRVz6GfHDs";
+                String videoId = "bn8MSLQ1Gsg";
                 youTubePlayer.loadVideo(videoId, 0);
             }
         });
 
+
+        //2번 영상
+        recipeViewLinearLayout2 = rootView.findViewById(R.id.recipe_view_linearLayout2);
+        LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        params1.bottomMargin = 30;
+
+        youTubePlayerView2 = new YouTubePlayerView(Health.this.getContext());
+        youTubePlayerView2.setLayoutParams(params1);
+        recipeViewLinearLayout2.addView(youTubePlayerView2);
+        getLifecycle().addObserver(youTubePlayerView2);
+
+        youTubePlayerView2.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(YouTubePlayer youTubePlayer2) {
+                String videoID2 = "wmRVz6GfHDs";
+                youTubePlayer2.loadVideo(videoID2, 0);
+            }
+        });
+
+        //3번 영상
+        recipeViewLinearLayout3 = rootView.findViewById(R.id.recipe_view_linearLayout3);
+        LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        params2.bottomMargin = 30;
+
+        youTubePlayerView3 = new YouTubePlayerView(Health.this.getContext());
+        youTubePlayerView3.setLayoutParams(params2);
+        recipeViewLinearLayout3.addView(youTubePlayerView3);
+        getLifecycle().addObserver(youTubePlayerView3);
+
+        youTubePlayerView3.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(YouTubePlayer youTubePlayer3) {
+                String videoID3 = "n3FWKzp8qFI";
+                youTubePlayer3.loadVideo(videoID3, 0);
+            }
+        });
+
+        //4번 영상
+        recipeViewLinearLayout4 = rootView.findViewById(R.id.recipe_view_linearLayout4);
+        LinearLayout.LayoutParams params3 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        params3.bottomMargin = 30;
+
+        youTubePlayerView4 = new YouTubePlayerView(Health.this.getContext());
+        youTubePlayerView4.setLayoutParams(params3);
+        recipeViewLinearLayout4.addView(youTubePlayerView4);
+        getLifecycle().addObserver(youTubePlayerView4);
+
+        youTubePlayerView4.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(YouTubePlayer youTubePlayer4) {
+                String videoID4 = "yIe5_WWgX-I";
+                youTubePlayer4.loadVideo(videoID4, 0);
+            }
+        });
+
+        //5번 영상
+        recipeViewLinearLayout5 = rootView.findViewById(R.id.recipe_view_linearLayout5);
+        LinearLayout.LayoutParams params4 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        params4.bottomMargin = 30;
+
+        youTubePlayerView5 = new YouTubePlayerView(Health.this.getContext());
+        youTubePlayerView5.setLayoutParams(params4);
+        recipeViewLinearLayout5.addView(youTubePlayerView5);
+        getLifecycle().addObserver(youTubePlayerView5);
+
+        youTubePlayerView5.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(YouTubePlayer youTubePlayer5) {
+                String videoID5 = "lOgtAR6_TlE";
+                youTubePlayer5.loadVideo(videoID5, 0);
+            }
+        });
+
+        //6번 영상
+        recipeViewLinearLayout6 = rootView.findViewById(R.id.recipe_view_linearLayout6);
+        LinearLayout.LayoutParams params5 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        params5.bottomMargin = 30;
+
+        youTubePlayerView6 = new YouTubePlayerView(Health.this.getContext());
+        youTubePlayerView6.setLayoutParams(params5);
+        recipeViewLinearLayout6.addView(youTubePlayerView6);
+        getLifecycle().addObserver(youTubePlayerView6);
+
+        youTubePlayerView6.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(YouTubePlayer youTubePlayer6) {
+                String videoID6 = "V0JbCxuh-_4";
+                youTubePlayer6.loadVideo(videoID6, 0);
+            }
+        });
 
         return rootView;
     }
--- app/src/main/java/com/example/sns_project2/training_tab/Running.java
+++ app/src/main/java/com/example/sns_project2/training_tab/Running.java
@@ -4,6 +4,8 @@ import static android.content.ContentValues.TAG;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -11,12 +13,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
 import com.example.sns_project2.R;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubeStandalonePlayer;
@@ -30,6 +35,7 @@ public class Running extends Fragment implements YouTubeThumbnailView.OnInitiali
     YouTubeThumbnailView youTubeThumbnailView;
     YouTubeThumbnailLoader youTubeThumbnailLoader;
 
+    ImageView imageviewThumbnail, imageviewThumbnail2;
 
     public String ID ="lKwZ2DU4P-A";
 
@@ -44,13 +50,42 @@ public class Running extends Fragment implements YouTubeThumbnailView.OnInitiali
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_running, container, false);
 
+        //1번 썸네일
         youTubeThumbnailView = rootView.findViewById(R.id.thumbnail);
         youTubeThumbnailView.initialize(API_KEY, Running.this);
-
         setListener();
+        
+
+        //2번 썸네일
+        imageviewThumbnail = rootView.findViewById(R.id.thumbnail_img);
+        Glide.with(getContext()).load("https://img.youtube.com/vi/VNQpP6C1fJg/0.jpg")
+                .thumbnail(0.1f)
+                .into(imageviewThumbnail);
+        imageviewThumbnail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=VNQpP6C1fJg"));
+                startActivity(intent);
+            }
+        });
+
+        //3번 썸네일
+        imageviewThumbnail2 = rootView.findViewById(R.id.thumbnail_img2);
+        Glide.with(getContext()).load("https://img.youtube.com/vi/swRNeYw1JkY/0.jpg")
+                .thumbnail(0.1f)
+                .into(imageviewThumbnail2);
+        imageviewThumbnail2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=swRNeYw1JkY"));
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
+
+
 
     private void setListener() {
         youTubeThumbnailView.setOnClickListener(new View.OnClickListener() {
@@ -70,6 +105,7 @@ public class Running extends Fragment implements YouTubeThumbnailView.OnInitiali
     @Override
     public void onInitializationSuccess(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader thumbnailLoader) {
 
+        //1번
         youTubeThumbnailLoader = thumbnailLoader;
         youTubeThumbnailLoader.setVideo(ID);
 
@@ -84,12 +120,11 @@ public class Running extends Fragment implements YouTubeThumbnailView.OnInitiali
                 Log.d(TAG, "onThumbnailfail");
             }
         });
-
-
     }
 
     @Override
     public void onInitializationFailure(YouTubeThumbnailView youTubeThumbnailView, YouTubeInitializationResult youTubeInitializationResult) {
         Log.d("에러", "에러");
     }
+
 }
